6b0aa85d21ae2ad9828dd085568483699919be9e
Mohit Saxena <Mohit.Saxena1@Geminisolutions.com>
2023-05-17 00:42:38 +0530

    registration form

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
-
-const Validation=()=>{
-    // for name validation
-    /*
-    const nameRegex = /^[a-zA-Z\s]+$/;
-    var fname=document.getElementById("firstName").value;
-    var lname=document.getElementById('lastName').value;
-    if(!(nameRegex.test(fname)))
-    {
-        alert("Please enter a valid First Name");
-    }
-    if(!(nameRegex.test(lname)))
-    {
-        alert("Please enter a valid Last Name");
-    }
-    */
+const Validation = () => {
     // for password validation
-    var passValue = document.getElementById("password").value
-    var confpassValue = document.getElementById("confirmPassword").value
-    if(passValue !== confpassValue) {
-       window.alert("Passwords does not match!");
-       
-    }  
-
-    // for mobile validation
-    const phoneNumberRegex = /^\d{10}$/;
-    const inputphone=document.getElementById('inputPhone');
-    if(inputphone==='' || !(phoneRegix.test(inputphone)))
-    {
-        alert("Please enter a valid Phone Number");
+    var passValue = document.getElementById("password").value;
+    var confpassValue = document.getElementById("confirmPassword").value;
+    if (passValue !== confpassValue) {
+      return "Passwords does not match!";
     }
-}
+  
+    // for mobile validation
+    const inputphone = document.getElementById("inputPhone").value;
+    const phoneNumberRegex = /^\d{10}$/;
+    console.log(phoneNumberRegex.test(inputphone));
+    if (inputphone === "" || !phoneNumberRegex.test(inputphone)) {
+      return "Please enter a valid Phone Number";
+    }
+  };
+  function validateForm(evt) {
+    res = Validation();
+    if (res) {
+      window.alert(res);
+      return false;
+    }
 
+    // retrieving data
+    var fname=document.getElementById('firstName').value;
+    var lname=document.getElementById('lastName').value;
+    var email=document.getElementById('email').value;
+    var password=document.getElementById('password').value;
+    var question=document.getElementById('question').value;
+    var answer=document.getElementById('inputAnswer').value;
+
+    // storing data
+
+    var frontName=localStorage.setItem('fname',fname);
+    var lastName=localStorage.setItem('lname',lname);
+    var emailId=localStorage.setItem('email',email);
+    var inputPassword=localStorage.setItem('password',password);
+    var securityQuestion=localStorage.setItem('question',question);
+    var securityAnswer=localStorage.setItem('inputAnswer',answer);
+
+
+    alert('Data inserted succesfully');
+    return true;
+  }
